d1187584a86087dd1ad2d36497f8766a64bfa5bf
yash <yashplorer@gmail.com>
2017-11-14 13:45:36 -0800

    ejection

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
@@ -6,17 +6,12 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Polo by MARCO</Text>
-        <Text>Revolutionizing</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-	<Text>Another change at 11:39 PM </Text>
-	<Text>THIS IS A CHANGE MADE AT 6:38 PM.</Text>
-        <Text>The College Dropout Best Kanye Album</Text> 
       </View>
     );
   }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
