ef6d1d64b5c94e86ad614fc343572d93b83a750d
andrewhongnguyen <andrewhongnguyen@gmail.com>
2017-11-14 19:45:09 -0800

    test

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {LogIn} from './logInScreen.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>Polo by MARCO</Text>
+        <LogIn/>
       </View>
     );
   }
