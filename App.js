f7552281432bae479a33b79ba0c54287e285a2a7
Jeremy Pham <jeremypham97@gmail.com>
2017-11-14 20:42:09 -0800

    First commit with functional map

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <MapView style={styles.map} initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}/>
         <Text style={styles.header}>Polo by MARCO</Text>
       </View>
     );
   }
 }
+
+
 
 
 
@@ -24,5 +33,9 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: 'black',
     fontWeight: 'bold',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
   }
 });
