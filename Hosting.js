26379c8c7757e54ecfdc9fef500e71de3c235123
jiahaowang5252@gmail.com <jiahaowang@jiahaos-mbp-2.attlocal.net>
2017-11-22 19:13:26 -0800

    Modal UI for input field

--- Hosting.js
+++ Hosting.js
@@ -1,132 +1,154 @@
 import React from 'react';
-import { Picker, StyleSheet, Text, View, TextInput } from 'react-native';
-import Button from 'apsl-react-native-button'
+import { Button, Picker, StyleSheet, Text, View, TextInput, Dimensions, TouchableOpacity } from 'react-native';
+import Modal from 'react-native-modal'; //Need to npm install react-native-modal --save
+import { StackNavigator } from 'react-navigation';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 //-1 represents empty values
-name = -1;
-phoneNum = -1;
-email = -1;
-password = -1;
 export default class Hosting extends React.Component {
     constructor(props){
         super(props);
         this.state =
         {
+            isModalVisible: false,
             eventName: "",
             category: "",
             description:"",
             time:""
         };
     }
+    _showModal = () => this.setState({ isModalVisible: true});
+
+    _hideModal = () => this.setState({ isModalVisible: false});
+
+
+
+    _onCreate = () =>{
+        info = [this.state.eventName, this.state.description, this.state.time];
+        for(i=0; i<info.length; i++){
+            console.log(info[i]);
+        }
+    };
 
     render() {
 
-        let emoji = [{
-            value: '1 hour',
-        }, {
-            value: '2 hour',
-        }, {
-            value: '3 hour',
-        }, {
-            value: '4 hour',
-        }, {
-            value: '5 hour'
-        }];
-
+        const { navigate } = this.props.navigation;
 
         return (
-
             <View style={styles.container}>
-                <Text style={styles.header}>Create your event</Text>
-
-
+                <Button
+                    style={{height: 50}}
+                    onPress={this._showModal}
+                    title="Host"
+                    color = "black"
+                />
                 {/*
-                    <EmojiPicker set='emojione' />
-                <EmojiPicker onClick={this.addEmoji} />
-                <EmojiPicker title='Pick your emoji…' emoji='point_up' />
-                <EmojiPicker style={{ position: 'absolute', bottom: '20px', right: '20px' }} />
-                <EmojiPicker i18n={{ search: 'Recherche', categories: { search: 'Résultats de recherche', recent: 'Récents' } }}
-                */}
-                <View style = {styles.firstRow}>
-                    <TextInput style={styles.name}
-                               placeholder = "Name of Event"
-                               returnKeyType = 'send'
-                               onEndEditing ={(text) => this.setState({eventName: text})}
-                    />
+                <Button
+                    onPress={this._hideModal()}
+                    title="Cancel"
+                    color = "black"
+                /> */}
+                <Modal isVisible={this.state.isModalVisible} backdropOpacity={0} style={styles.bottomModal}
+                        animationIn ="slideInUp">
+                    <View style={styles.modalContentContainer}>
+                        <View style={{flexDirection:"row", alignItems:"center"}}>
+                            <Text style={styles.header}>Create your event</Text>
+                            <Button
+                                onPress={() => this.setState({isModalVisible: false})}
+                                title="Cancel"
+                                color = "black"
+                            />
+                        </View>
 
-                    {/*
+
+                        {/*
+                            <EmojiPicker set='emojione' />
+                        <EmojiPicker onClick={this.addEmoji} />
+                        <EmojiPicker title='Pick your emoji…' emoji='point_up' />
+                        <EmojiPicker style={{ position: 'absolute', bottom: '20px', right: '20px' }} />
+                        <EmojiPicker i18n={{ search: 'Recherche', categories: { search: 'Résultats de recherche', recent: 'Récents' } }}
+                        */}
+                        <View style = {styles.firstRow}>
+                            <TextInput style={styles.name}
+                                       placeholder = "Name of Event"
+                                       returnKeyType = 'send'
+                                       onEndEditing ={(text) => this.setState({eventName: text})}
+                            />
+
+                            {/*
+                                <Dropdown style={styles.categories}
+                                          label='Favorite Fruit'
+                                          data={data}/>
+                            */}
+
+
+                            {/*
+                                <Picker
+                                style = {styles.picker}
+                                selectedValue={this.state.language}
+                                onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}
+                                mode = 'dropdown'>
+                                <Picker.Item label="Java" value="java" />
+                                <Picker.Item label="JavaScript" value="js" />
+                                </Picker>
+                            */}
+                        </View>
+
+                        {/*
                         <Dropdown style={styles.categories}
                                   label='Favorite Fruit'
-                                  data={data}/>
-                    */}
+                                  data={data}
+
+                                  <Dropdown style={styles.when}
+                                      label='In how many hours?'
+                                      data={time}/>
+                        />
+                        */}
 
 
-                    {/*
-                        <Picker
-                        style = {styles.picker}
-                        selectedValue={this.state.language}
-                        onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}
-                        mode = 'dropdown'>
-                        <Picker.Item label="Java" value="java" />
-                        <Picker.Item label="JavaScript" value="js" />
-                        </Picker>
-                    */}
-                </View>
 
-                {/*
-                <Dropdown style={styles.categories}
-                          label='Favorite Fruit'
-                          data={data}
+                        <View style={styles.secondRow}>
+                            <TextInput style={styles.categories}
+                                       placeholder = "Category"
+                                       returnKeyType = 'send'
+                                       onChangeText ={(text) => this.setState({text})}
+                            />
+                        </View>
 
-                          <Dropdown style={styles.when}
-                              label='In how many hours?'
-                              data={time}/>
-                />
-                */}
+                        <View style={styles.thirdRow}>
+                            <TextInput style={styles.description}
+                                       placeholder = "Event Description"
+                                       returnKeyType = 'send'
+                                       onChangeText ={(text) => this.setState({description: text})}
+                            />
+                        </View>
 
-                <View style={styles.secondRow}>
-                    <TextInput style={styles.categories}
-                               placeholder = "Category"
-                               returnKeyType = 'send'
-                               onChangeText ={(text) => this.setState({text})}
-                    />
-                </View>
-
-                <View style={styles.thirdRow}>
-                    <TextInput style={styles.description}
-                               placeholder = "Event Description"
-                               returnKeyType = 'send'
-                               onChangeText ={(text) => this.setState({description: text})}
-                    />
-                </View>
-
-                <View style={styles.fourthRow}>
-                    <Picker
-                        style = {styles.picker}
-                        itemStyle = {styles.item}
-                        selectedValue={this.state.time}
-                        onValueChange={(itemValue, itemIndex) => this.setState({time: itemValue})}
-                        mode = 'dropdown'>
-                        <Picker.Item label="In 1 Hour" value="1" />
-                        <Picker.Item label="In 2 Hours" value="2" />
-                        <Picker.Item label="In 3 Hours" value="3" />
-                        <Picker.Item label="In 4 Hours" value="4" />
-                        <Picker.Item label="In 5 Hours" value="5" />
-                    </Picker>
-                </View>
-                <View style={{flexDirection:"row", justifyContent:"flex-end"}}>
-                    <Button onPress= {onCreate()} style={styles.create}>
-                        <Text style={{fontSize: 18}}>Create</Text>
-                    </Button>
-                </View>
-
+                        <View style={styles.fourthRow}>
+                            <Picker
+                                style = {styles.picker}
+                                itemStyle = {styles.item}
+                                selectedValue={this.state.time}
+                                onValueChange={(itemValue, itemIndex) => this.setState({time: itemValue})}
+                                mode = 'dropdown'>
+                                <Picker.Item label="In 1 Hour" value="1" />
+                                <Picker.Item label="In 2 Hours" value="2" />
+                                <Picker.Item label="In 3 Hours" value="3" />
+                                <Picker.Item label="In 4 Hours" value="4" />
+                                <Picker.Item label="In 5 Hours" value="5" />
+                            </Picker>
+                        </View>
+                        <View style={{flexDirection:"row", justifyContent:"flex-end"}}>
+                            <Button onPress= {this._onCreate()} title="Create" style={styles.create}/>
+                        </View>
+                    </View>
+                </Modal>
             </View>
         );
     }
 }
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
@@ -135,7 +157,7 @@ const styles = StyleSheet.create({
         fontSize: 30,
         color: "black",
         fontWeight: "bold",
-        padding: 20
+        padding: 12
     },
 
     firstRow:{
@@ -186,6 +208,21 @@ const styles = StyleSheet.create({
         height: 100,
         fontSize: 19,
         borderColor: "#ffffff"
+    },
+    bottomModal:{
+        justifyContent: 'flex-end',
+        padding: 0,
+        margin: 0,
+    },
+
+    modalContentContainer: {
+        borderRadius: 20,
+        alignSelf: 'center',
+        alignItems: 'center',
+        height: 345,
+        backgroundColor: "white",
+        width: Dimensions.get('window').width,
+        margin: 0,
     }
 
 
@@ -193,9 +230,3 @@ const styles = StyleSheet.create({
 });
 
 
-onCreate = () =>{
-    info = [this.eventName, this.description, this.time];
-    for(i=0; i<info.length; i++){
-        console.log(info[i]);
-    }
-}
