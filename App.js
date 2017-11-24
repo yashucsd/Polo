38ae579a675b6b0c2db55db4e2a1aeea2d35a805
Jeremy Pham <jeremypham97@gmail.com>
2017-11-24 01:35:56 -0800

    Finished adding geolocation and created markers to match UI

--- App.js
+++ App.js
@@ -1,82 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import { Map } from './Map.js';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      region: {
-        latitude: 13.761,
-        longitude: 100.2,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005
-      },
-      markers:[
-        {
-          latlng: {latitude: 13.76, longitude: 100.2},
-          title: "Victory Monument", description: "A large monument"
-        },
-        {
-          latlng: {latitude: 13.763681, longitude: 100.2},
-          title: "Second Marker", description: "Pin number 2s"
-        },
-        {
-          latlng: {latitude: 13.763681, longitude: 100.2},
-          title: "Third Marker", description: "Pin number 3 same as 2"
-        }, 
-        {
-          latlng: {latitude: 13.764081, longitude: 100.2005},
-          title: "Fourth Marker", description: "This is the fourth pin"
-        }, 
-      ]
-    };
-    this.onRegionChange = this.onRegionChange.bind(this);
-  }
-
-  onRegionChange(region) {
-    this.setState({ region });
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <MapView style={styles.map}
-          mapType="standard"
-          
-          /*showsUserLocation={true}
-          //followsUserLocation{true} 
-          showsCompass={false}
-          showsPointsOfInterest={false}*/
-          region={this.state.region}
-          onRegionChange={this.onRegionChange}
-        >
-        {this.state.markers.map((marker, i) => (
-          <MapView.Marker
-            key={i}
-            coordinate={marker.latlng}
-            title={marker.title}
-            description={marker.description}
-          />
-        ))}
-          
-        </MapView>
-        <View style={styles.container}>
-          <Text style={styles.header}>
-            Latitude: {this.state.region.latitude}{'\n'}
-            Longitude: {this.state.region.longitude}{'\n'}
-            LatitudeDelta: {this.state.region.latitudeDelta}{'\n'}
-            LongitudeDelta: {this.state.region.longitudeDelta}
-          </Text>
-        </View>
+        <Map/>
       </View>
     );
   }
 }
-
-
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -85,13 +19,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    fontSize: 18,
-    color: 'black',
-    fontWeight: 'bold',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: 400,
-  }
+  
 });
---
+++ Map.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+import emoji from 'node-emoji';
+
+//import markers from './markers.json';
+
+export class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 32.8804,
+        longitude: -117.2375,
+        latitudeDelta: 0.008,
+        longitudeDelta: 0.008,
+      },
+      
+      markers:[
+        {
+          latlng: {latitude: 32.8804, longitude: -117.2375},
+          title: "Geisel Library", description: "Come study!", image: "books"
+        },
+        {
+          latlng: {latitude: 32.884, longitude: -117.2381},
+          title: "RIMAC", description: "Playing basketball", image: "basketball"
+        },
+        {
+          latlng: {latitude: 32.8801, longitude: -117.2340},
+          title: "Warren Dorms", description: "Playing video games", image: "video_game"
+        }, 
+        {
+          latlng: {latitude: 32.8803, longitude: -117.241},
+          title: "Marshall College", description: "Walking my dog!", image: "dog"
+        }, 
+      ] // end of markers
+    }; // end of this.state
+    this.onRegionChange = this.onRegionChange.bind(this);
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* Setting attributes for the MapView */}
+        <MapView style={styles.map}
+          mapType="standard"
+          showsUserLocation={true}
+          showsCompass={true}
+          showsPointsOfInterest={true}
+          showsMyLocationButton={true}
+          toolbarEnabled={true}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        >
+        {/* Information for each marker is used to create them 
+            (Child of MapView) */}
+        {this.state.markers.map((marker, i) => (
+          <MapView.Marker
+            key={i}
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}>
+
+            {/* This is a custom view to show an emoji and its BG 
+                (Child of MapView.Marker) */}
+            <View style={styles.markerBG}>
+              <Text style={styles.markerEmoji}>{emoji.get(marker.image)}</Text>
+            </View>
+
+          </MapView.Marker>
+        ))}
+        </MapView>
+        {/* <View style={styles.container}>
+          <Text style={styles.header}>
+            Latitude: {this.state.region.latitude}{'\n'}
+            Longitude: {this.state.region.longitude}{'\n'}
+            LatitudeDelta: {this.state.region.latitudeDelta}{'\n'}
+            LongitudeDelta: {this.state.region.longitudeDelta}
+          </Text>
+        </View> */}
+      </View>
+    );
+  }
+}
+
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d3d3d3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 18,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  markerBG: {
+    backgroundColor: 'rgba(52, 52, 52, 0.75)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    height: 60,
+    width: 60, 
+    padding: 5,
+    borderRadius: 30,
+  },
+  markerEmoji: {
+    fontSize: 30,
+    color: 'black'
+  },
+});
