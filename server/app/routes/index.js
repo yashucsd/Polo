7b0daff5c27516c374386904fcd82b87a56a9e50
Garrett Crawford <ochirasu@gmail.com>
2017-11-28 17:57:51 -0800

    setup structure for db code

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,6 +1,11 @@
-const testUserRoutes = require('./test_user_routes');
+const testUserRoutes = require('./test_user_routes.js');
+const userRoutes = require('./users_routes.js')
+const activitiesRoutes = require('./activities_routes.js');
+const preferencesRoutes = require('./preferences_routes.js')
 
 module.exports = function(app, db) {
     testUserRoutes(app, db);
-    // other route groups may go here in future
+    userRoutes(app, db);
+    activitiesRoutes(app, db);
+    preferencesRoutes(app, db);
 }
