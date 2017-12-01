d501f40611ea1c76df2b54a50dd73f63a81b9cc9
Brendon Ho <bqh001@ucsd.edu>
2017-11-30 17:45:29 -0800

    Trying to merge

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import { Map } from './Map.js';
+import ActivityDetails from './ActivityDetails.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Map/>
-      </View>
+      <ActivityDetails/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-});
