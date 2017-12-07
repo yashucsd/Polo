1e17cf7b16cf7be2f3c002d806ce94b0c4d6c2c4
yash <yashplorer@gmail.com>
2017-12-06 21:21:06 -0800

    UI improvements to user preferences for ios

--- userPreferenceScreen.js
+++ userPreferenceScreen.js
@@ -91,73 +91,35 @@ export default class UserPreference extends React.Component {
     return (
       <ScrollView>
         <View style={styles.container}>
-          {/*User Preferences*/}
-          <Text
-            style={{
-              fontSize: 40,
-              fontWeight: "bold",
-              color: "black",
-              paddingLeft: width / 15,
-              fontWeight: "bold",
-              marginBottom: 50
-            }}
-          >
-            User Preferences
-          </Text>
-
-          <Text
-            style={{
-              fontWeight: "bold",
-              fontSize: 22,
-              color: "black",
-              paddingLeft: width / 15,
-              marginBottom: 10
-            }}
-          >
-            Notifications
-          </Text>
-
-          <View
-            style={{
-              backgroundColor: "white",
-              justifyContent: "space-between"
-            }}
-          >
-            <View style={styles.whiteBoxSection}>
+          <Text style={styles.heading}>User Preferences</Text>
+          <Text style={styles.subheading}>Notifications</Text>
+          <View style={styles.sectionBox}>
+            <View style={styles.notificationListItem}>
               <Text style={styles.textFontB}>All Notifications</Text>
-
               <Switch
-                onValueChange={value =>
-                  this.setState({ allNotifications: value })
-                }
+                style={styles.switch}
+                onValueChange={value => this.setState({ allNotifications: value })}
                 value={this.state.allNotifications}
               />
             </View>
-            <Text style={styles.line}> </Text>
-
-            <View style={styles.whiteBoxSection}>
+            <View style={styles.border} />
+            <View style={styles.notificationListItem}>
               <Text style={styles.textFontB}>Popular Activities</Text>
-
               <Switch
-                onValueChange={value =>
-                  this.setState({ popularActivities: value })
-                }
+                style={styles.switch}
+                onValueChange={value => this.setState({ popularActivities: value })}
                 value={this.state.popularActivities}
                 disabled={!this.state.allNotifications}
               />
             </View>
-
-            <Text style={styles.line}> </Text>
-
-            <View style={styles.whiteBoxSection}>
+            <View style={styles.border} />
+            <View style={styles.notificationListItem}>
               <Text style={styles.textFontB}>Activity Radius</Text>
-
               <Text style={styles.textFontC}>{this.state.radius} miles</Text>
             </View>
-
             <View style={{ marginBottom: 25 }}>
               <Slider
-                style={{ width: width * 0.8, alignSelf: "center" }}
+                style={styles.slider}
                 step={1}
                 minimumValue={0}
                 maximumValue={50}
@@ -169,190 +131,110 @@ export default class UserPreference extends React.Component {
           </View>
 
           <View style={{ margin: 15, paddingTop: 35 }}>
-            <Text style={{ fontSize: 20, fontWeight: "bold", color: "black" }}>
-              {" "}
+            <Text style={{fontWeight: "bold", fontSize: 22, color: "black", marginBottom: 10,}}>
               Categories
             </Text>
             <Text style={styles.textFont}>
-              {" "}
-              Choose the categories of activities you want
-            </Text>
-            <Text style={styles.textFont}>
-              {" "}
-              to see on the map and be notified about{" "}
+              Choose the categories of activities you want{"\n"}
+              to see on the map and be notified about
             </Text>
           </View>
 
-          <View
-            style={{
-              backgroundColor: "white",
-              justifyContent: "space-between",
-              paddingBottom: 30,
-              marginBottom: 30
-            }}
-          >
-            <View style={styles.yashMargin}>
-              <View style={styles.markerBG}>
-                <Text style={styles.emoteSmall}>
-                  {renderIf(this.state.emoji1)(
-                    <Image source={require("./pictures/checkmark.png")} />
-                  )}
+          <View style={{ backgroundColor: "white", justifyContent: "space-between", paddingBottom: 30, marginBottom: 30}}>
+            
+            <View style={styles.categoryRow}>
+
+              <View style={styles.categoryButton}>
+                <Text style={styles.checkbox}>
+                  {renderIf(this.state.emoji1)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
                 </Text>
-                <TouchableHighlight
-                  style={{}}
-                  onPress={() => this.flipEmoji(1)}
-                  underlayColor={"transparent"}
-                >
-                  <Text style={styles.emote}>{emoji.get("basketball")}</Text>
+                <TouchableHighlight onPress={() => this.flipEmoji(1)} underlayColor={"transparent"}>
+                  <Text style={styles.categoryEmoji}>{emoji.get("basketball")}</Text>
                 </TouchableHighlight>
               </View>
 
-              <View style={styles.markerBG}>
-                <Text style={styles.emoteSmall}>
-                  {renderIf(this.state.emoji2)(
-                    <Image
-                      source={require("./pictures/checkmark.png")}
-                      style={{ position: "absolute" }}
-                    />
-                  )}
+              <View style={styles.categoryButton}>
+                <Text style={styles.checkbox}>
+                  {renderIf(this.state.emoji2)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
                 </Text>
-                <TouchableHighlight
-                  style={{}}
-                  onPress={() => this.flipEmoji(2)}
-                  underlayColor={"transparent"}
-                >
-                  <Text style={styles.emote}>{emoji.get("books")}</Text>
+                <TouchableHighlight onPress={() => this.flipEmoji(2)} underlayColor={"transparent"}>
+                  <Text style={styles.categoryEmoji}>{emoji.get("books")}</Text>
                 </TouchableHighlight>
               </View>
-              <View style={styles.markerBG}>
-                <Text style={styles.emoteSmall}>
-                  {renderIf(this.state.emoji3)(
-                    <Image
-                      source={require("./pictures/checkmark.png")}
-                      style={{ position: "absolute" }}
-                    />
-                  )}
+
+              <View style={styles.categoryButton}>
+                <Text style={styles.checkbox}>
+                  {renderIf(this.state.emoji3)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
                 </Text>
-                <TouchableHighlight
-                  style={{}}
-                  onPress={() => this.flipEmoji(3)}
-                  underlayColor={"transparent"}
-                >
-                  <Text style={styles.emote}>{emoji.get("hamburger")}</Text>
+                <TouchableHighlight onPress={() => this.flipEmoji(3)} underlayColor={"transparent"}>
+                  <Text style={styles.categoryEmoji}>{emoji.get("hamburger")}</Text>
                 </TouchableHighlight>
               </View>
             </View>
-            <View style={styles.yashMargin}>
-              <View style={styles.markerBG}>
-                <Text style={styles.emoteSmall}>
-                  {renderIf(this.state.emoji4)(
-                    <Image
-                      source={require("./pictures/checkmark.png")}
-                      style={{ position: "absolute" }}
-                    />
-                  )}
+
+            <View style={styles.categoryRow}>
+
+              <View style={styles.categoryButton}>
+                <Text style={styles.checkbox}>
+                  {renderIf(this.state.emoji4)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
                 </Text>
-                <TouchableHighlight
-                  style={{}}
-                  onPress={() => this.flipEmoji(4)}
-                  underlayColor={"transparent"}
-                >
-                  <Text style={styles.emote}>
-                    {emoji.get("snow_capped_mountain")}
-                  </Text>
+                <TouchableHighlight onPress={() => this.flipEmoji(4)} underlayColor={"transparent"}>
+                  <Text style={styles.categoryEmoji}>{emoji.get("snow_capped_mountain")}</Text>
                 </TouchableHighlight>
               </View>
-              <View style={styles.markerBG}>
-                <Text style={styles.emoteSmall}>
-                  {renderIf(this.state.emoji5)(
-                    <Image
-                      source={require("./pictures/checkmark.png")}
-                      style={{ position: "absolute" }}
-                    />
-                  )}
+
+              <View style={styles.categoryButton}>
+                <Text style={styles.checkbox}>
+                  {renderIf(this.state.emoji5)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
                 </Text>
-                <TouchableHighlight
-                  style={{}}
-                  onPress={() => this.flipEmoji(5)}
-                  underlayColor={"transparent"}
-                >
-                  <Text style={styles.emote}>{emoji.get("shopping_bags")}</Text>
+                <TouchableHighlight onPress={() => this.flipEmoji(5)} underlayColor={"transparent"}>
+                  <Text style={styles.categoryEmoji}>{emoji.get("shopping_bags")}</Text>
                 </TouchableHighlight>
               </View>
-              <View style={styles.markerBG}>
-                <Text style={styles.emoteSmall}>
-                  {renderIf(this.state.emoji6)(
-                    <Image
-                      source={require("./pictures/checkmark.png")}
-                      style={{ position: "absolute" }}
-                    />
-                  )}
+
+              <View style={styles.categoryButton}>
+                <Text style={styles.checkbox}>
+                  {renderIf(this.state.emoji6)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
                 </Text>
-                <TouchableHighlight
-                  style={{}}
-                  onPress={() => this.flipEmoji(6)}
-                  underlayColor={"transparent"}
-                >
-                  <Text style={styles.emote}>{emoji.get("snowflake")}</Text>
+                <TouchableHighlight onPress={() => this.flipEmoji(6)} underlayColor={"transparent"}>
+                  <Text style={styles.categoryEmoji}>{emoji.get("snowflake")}</Text>
                 </TouchableHighlight>
               </View>
             </View>
-            <View style={styles.yashMargin}>
-              <View style={styles.markerBG}>
-                <Text style={styles.emoteSmall}>
-                  {renderIf(this.state.emoji7)(
-                    <Image
-                      source={require("./pictures/checkmark.png")}
-                      style={{ position: "absolute" }}
-                    />
-                  )}
+
+            <View style={styles.categoryRow}>
+
+              <View style={styles.categoryButton}>
+                <Text style={styles.checkbox}>
+                  {renderIf(this.state.emoji7)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
                 </Text>
-                <TouchableHighlight
-                  style={{}}
-                  onPress={() => this.flipEmoji(7)}
-                  underlayColor={"transparent"}
-                >
-                  <Text style={styles.emote}>{emoji.get("video_game")}</Text>
+                <TouchableHighlight onPress={() => this.flipEmoji(7)} underlayColor={"transparent"}>
+                  <Text style={styles.categoryEmoji}>{emoji.get("video_game")}</Text>
                 </TouchableHighlight>
               </View>
-              <View style={styles.markerBG}>
-                <Text style={styles.emoteSmall}>
-                  {renderIf(this.state.emoji8)(
-                    <Image
-                      source={require("./pictures/checkmark.png")}
-                      style={{ position: "absolute" }}
-                    />
-                  )}
+
+              <View style={styles.categoryButton}>
+                <Text style={styles.checkbox}>
+                  {renderIf(this.state.emoji8)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
                 </Text>
-                <TouchableHighlight
-                  style={{}}
-                  onPress={() => this.flipEmoji(8)}
-                  underlayColor={"transparent"}
-                >
-                  <Text style={styles.emote}>{emoji.get("tada")}</Text>
+                <TouchableHighlight onPress={() => this.flipEmoji(8)} underlayColor={"transparent"}>
+                  <Text style={styles.categoryEmoji}>{emoji.get("tada")}</Text>
                 </TouchableHighlight>
               </View>
-              <View style={styles.markerBG}>
-                <Text style={styles.emoteSmall}>
-                  {renderIf(this.state.emoji9)(
-                    <Image
-                      source={require("./pictures/checkmark.png")}
-                      style={{ position: "absolute" }}
-                    />
-                  )}
+
+              <View style={styles.categoryButton}>
+                <Text style={styles.checkbox}>
+                  {renderIf(this.state.emoji9)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
                 </Text>
-                <TouchableHighlight
-                  style={{}}
-                  onPress={() => this.flipEmoji(9)}
-                  underlayColor={"transparent"}
-                >
-                  <Text style={styles.emote}>{emoji.get("weight_lifter")}</Text>
+                <TouchableHighlight onPress={() => this.flipEmoji(9)} underlayColor={"transparent"}>
+                  <Text style={styles.categoryEmoji}>{emoji.get("weight_lifter")}</Text>
                 </TouchableHighlight>
               </View>
             </View>
+
           </View>
 
-          <View>
+          <View style={styles.signOut}>
             <Button
               onPress={() => this.props.navigation.navigate("LogInScreen")}
               title="Sign Out"
@@ -365,7 +247,49 @@ export default class UserPreference extends React.Component {
   }
 }
 
+// props.checkboxState = this.state.emoji1
+// props.categoryId = 1
+// props.categoryEmoji = "basketball"
+
+
+function CategoryButton(props) {
+  return (
+    <View style={styles.categoryButton}>
+      <Text style={styles.checkbox}>
+        {renderIf(props.checkboxState)(<Image style={styles.checkboxImg} source={require("./pictures/checkmark.png")} />)}
+      </Text>
+      <TouchableHighlight onPress={() => this.flipEmoji(props.categoryId)} underlayColor={"transparent"}>
+        <Text style={styles.categoryEmoji}>{emoji.get(props.categoryEmoji)}</Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
+  heading: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "black",
+    paddingLeft: 15,
+    fontWeight: "bold",
+    marginBottom: 50,
+    marginTop: 25,
+  },
+  subheading: {
+    fontWeight: "bold",
+    fontSize: 22,
+    color: "black",
+    marginBottom: 10,
+    paddingLeft: 15,
+  },
+  switch: {
+    marginTop: 22,
+    marginRight: 10
+  },
+  slider: {
+     width: width * 0.8,
+     alignSelf: "center" 
+  },
   container: {
     flex: 1,
     backgroundColor: "#F7F7F7FF"
@@ -378,21 +302,24 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 25
   },
-  emote: {
+  categoryEmoji: {
     fontSize: Math.round(height / 25),
-    color: "green",
-    paddingBottom: 25
+    marginTop: 25,
   },
-  emoteSmall: {
-    fontSize: Math.round(height / 80),
-    color: "green",
-    paddingLeft: 50
+  checkbox: {
+    position: "absolute",
+    right: 0,
+    backgroundColor: "transparent",
   },
-  emoteRow: {
+  checkboxImg: {
+    height: 27,
+    width: 27,
+  },
+  categoryEmojiRow: {
     flex: 2,
     flexDirection: "row"
   },
-  emoteRowSmall: {
+  categoryEmojiRowSmall: {
     flex: 1,
     flexDirection: "row"
   },
@@ -413,21 +340,25 @@ const styles = StyleSheet.create({
     paddingTop: 25,
     paddingRight: 15
   },
-  whiteBoxSection: {
+  notificationListItem: {
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
-  line: {
-    borderBottomColor: "black",
+  border: {
+    borderBottomColor: "#E2E2E2",
     borderStyle: "solid",
-    borderWidth: 0.25,
-    color: "#E2E2E2",
-    fontSize: 0,
+    borderBottomWidth: 1,
     alignSelf: "flex-end",
     width: width * 0.95
   },
 
-  markerBG: {
+  signOut: {
+    backgroundColor: "white",
+    padding: 5,
+    marginBottom: 50,
+  },
+
+  categoryButton: {
     backgroundColor: "#333",
     borderColor: "black",
     height: 84,
@@ -438,7 +369,12 @@ const styles = StyleSheet.create({
     justifyContent: "space-between"
   },
 
-  yashMargin: {
+  sectionBox: {
+    backgroundColor: "white",
+    justifyContent: "space-between"
+  },
+
+  categoryRow: {
     flexDirection: "row",
     justifyContent: "space-between",
     marginTop: 30,
